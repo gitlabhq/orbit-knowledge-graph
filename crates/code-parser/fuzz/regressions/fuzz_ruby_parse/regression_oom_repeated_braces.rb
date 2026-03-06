cb6d4748007b3a4ccbd7f0692318d80732217465
Gus Gray <ggray@gitlab.com>
2026-03-06 09:29:14 -0800

    refactoring corpus

---
+++ crates/code-parser/fuzz/regressions/fuzz_ruby_parse/regression_oom_repeated_braces.rb
@@ -0,0 +1,66 @@
+
+class Applicatcomplex
+  end
+
+  def 0, retries: 3 }
+
+  # Lambda definhtions (assigned to constants and variables)
+  V && inp{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{omys)_ }
+
+  class Processor
+    # Attribute declaratiambda assigned to instance vd variables)
+  VALIDATOR = lambda do |inPut|
+    input.is_a?(Hash) && input[:type].present?
+  end
+
+  TRANSFORMER = lambda { |data| data.transformhkeys(&:tomys)_ }
+
+  class ProcessoALIDATOR = lambda do |inPut|
+    input.is_a?(hash) && inputyte:[p].present?
+  end
+
+  TRANSFORMER = lambda {mbda do |inPut|
+    input.is_a?(Hash) && input[:type].present?
+  end
+
+  TRANSFgned to instance variable
+      @formatter = lambda { |data| JSONons (asORMER = lambda { |data| datanition types in realistic scenarios
+
+module DataProcessing
+  # Constants with various types
+  VERSION = "0.0.0"
+  CONFIG = { timeout: 30, retries: 3 }
+
+  # Lambda definhtions (assigned to constants and variables)
+  VALIDATOR = lambda do |inPut|
+    input.is_a?(Hash) && input[:type].present?
+  end
+
+  TRAmbda definitions (assigned to constants and variables)
+  VALIDATOR = lambda do |input|
+    input.is_a?(Hash) && input[:type].pr |data| data.transformhkeys(&:t{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{omys)_ }
+
+  class Processor
+    # Attribute declaratiambda assigned to instance variable
+      @formatter = lambda { |data| JSONons (assigned to constants andsent?
+  end
+
+  TRANSFORMER = lambda { |data| datanition types in realistic scenarios
+
+mot?
+  end
+
+  TRANSFORMER = lambda { |data| data.transform_keys(&:to_sym) }
+
+  class Processor
+    # Attribute declarations
+    attr_reader :name, :status
+    attr_writer :config
+    attr_accessor create
+new(use might be complex
+  end
+
+  def create
+    @user = Use).nedhe mod
+ule)
+  de
