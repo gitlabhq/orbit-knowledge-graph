a070620cbb5ce9c46379f76bf1548c8a8b6fd121
michaelusa <musachenko@gitlab.com>
2026-03-19 23:26:45 -0700

    fix: init

--- crates/integration-testkit/src/lib.rs
+++ crates/integration-testkit/src/lib.rs
@@ -63,28 +63,53 @@ macro_rules! run_subtests {
 #[macro_export]
 macro_rules! run_subtests_shared {
     ($ctx:expr, $($test_fn:path),+ $(,)?) => {{
-        use futures::stream::StreamExt as _;
-
         let _concurrency: usize = std::env::var("SUBTEST_CONCURRENCY")
             .ok()
             .and_then(|v| v.parse().ok())
             .unwrap_or(8);
 
-        let _futs = vec![
-            $(
-                Box::pin(async {
-                    let _t = std::time::Instant::now();
-                    eprintln!("--- {}", stringify!($test_fn));
-                    $test_fn($ctx).await;
-                    eprintln!("    {} {:.2?}", stringify!($test_fn), _t.elapsed());
-                }) as std::pin::Pin<Box<dyn std::future::Future<Output = ()> + '_>>,
-            )+
-        ];
+        let _sem = std::sync::Arc::new(tokio::sync::Semaphore::new(_concurrency));
+        let mut _handles: Vec<(&str, tokio::task::JoinHandle<()>)> = Vec::new();
 
-        futures::stream::iter(_futs)
-            .buffer_unordered(_concurrency)
-            .collect::<Vec<()>>()
-            .await;
+        $(
+            let _permit = _sem.clone().acquire_owned().await.unwrap();
+            let _name: &str = stringify!($test_fn);
+            // SAFETY: ctx outlives all spawned tasks because we join below.
+            // The borrow checker can't see this, so we use a raw pointer.
+            let _ctx_ptr = $ctx as *const _ as usize;
+            let _handle = tokio::task::spawn(async move {
+                let _ctx_ref = unsafe { &*(_ctx_ptr as *const _) };
+                let _t = std::time::Instant::now();
+                eprintln!("--- {}", _name);
+                $test_fn(_ctx_ref).await;
+                eprintln!("    {} {:.2?}", _name, _t.elapsed());
+                drop(_permit);
+            });
+            _handles.push((_name, _handle));
+        )+
+
+        let mut _failed: Vec<String> = Vec::new();
+        for (_name, _handle) in _handles {
+            match _handle.await {
+                Ok(()) => {}
+                Err(e) => {
+                    let msg = if let Some(s) = e.into_panic().downcast_ref::<String>() {
+                        s.clone()
+                    } else {
+                        "panic (see stderr)".to_string()
+                    };
+                    _failed.push(format!("{}: {}", _name, msg));
+                }
+            }
+        }
+
+        if !_failed.is_empty() {
+            panic!(
+                "\n{} subtest(s) failed:\n{}",
+                _failed.len(),
+                _failed.iter().map(|f| format!("  - {f}")).collect::<Vec<_>>().join("\n")
+            );
+        }
     }};
 }
 
