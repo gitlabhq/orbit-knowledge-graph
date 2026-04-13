e8948bcc4a1a0d38326adb4173d3360ade653a2f
michaelusa <musachenko@gitlab.com>
2026-04-12 22:20:51 -0700

    refactor: remove rules_resolver_tests.rs, superseded by YAML Cypher suites

--- crates/code-graph/src/v2/mod.rs
+++ crates/code-graph/src/v2/mod.rs
@@ -1,7 +1,5 @@
 pub mod lang_rules;
 mod pipeline;
 pub mod resolvers;
-#[cfg(test)]
-mod rules_resolver_tests;
 
 pub use pipeline::{GenericPipeline, LanguagePipeline, Pipeline, PipelineConfig, PipelineResult};
