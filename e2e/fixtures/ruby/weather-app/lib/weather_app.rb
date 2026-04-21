f0b90522ebda3f4bf9c0431b31117ae0588e99ca
Bohdan Parkhomchuk <bparkhomchuk@gitlab.com>
2026-04-20 23:33:54 -0500

    test(e2e): add ruby and java weather fixtures

---
+++ e2e/fixtures/ruby/weather-app/lib/weather_app.rb
@@ -0,0 +1,8 @@
+require_relative "weather_app/forecast"
+require_relative "weather_app/weather_service"
+require_relative "weather_app/formatter"
+require_relative "weather_app/cli"
+
+module WeatherApp
+  VERSION = "0.1.0".freeze
+end
