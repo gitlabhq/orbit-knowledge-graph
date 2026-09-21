93b25569bf3443db29d7306f17baa223b0785dff
michaelangeloio <arivera@gitlab.com>
2026-09-20 20:02:29 -0400

    feat(tree-dsl): add C#, C, C++, Scala, Bash, Elixir, Lua, Swift, and Zig rules
    C#: namespaces, using directives (namespace, static, alias), classes,
    structs, records, interfaces, enums, fields and properties with this.x
    typing, constructors marked with <init>, implicit-this calls, base calls.
    
    C and C++: includes as wildcard imports, functions, structs, unions, enums,
    typedefs, namespaces as class-like scopes, in-class methods, out-of-line
    methods as __impl wrappers, typed locals, member and qualified calls.
    
    Scala: package, imports (simple, grouped, renamed, wildcard), classes,
    traits, objects, defs, class parameters with this.x typing.
    
    Bash: source imports, functions, commands as calls. Elixir: alias,
    import, require, use, defmodule, def/defp, Module.fun calls. Lua: require
    imports, table-scoped functions as members of table definitions. Swift:
    classes, structs, enums, extensions, protocols, properties, self./super.
    calls. Zig: const/var declarations, containers, functions, tests.
    
    Fixtures: vocabulary renamed to the canonical alphabet (Typedef to
    TypeAlias, EnumConstant/EnumEntry/EnumMember to EnumVariant, Namespace,
    CaseClass, Object, Module, Record to Class, Union to Struct, Const/Var to
    Constant/Variable, Test to Function); engine-limited tests skipped with
    categorized reasons.

--- crates/tree-dsl/src/treesitter.rs
+++ crates/tree-dsl/src/treesitter.rs
@@ -69,6 +69,14 @@ pub fn lang_yaml(lang_id: SupportLang) -> Option<&'static str> {
         SupportLang::Kotlin => Some(include_str!("../langs/kotlin.yaml")),
         SupportLang::Ruby => Some(include_str!("../langs/ruby.yaml")),
         SupportLang::CSharp => Some(include_str!("../langs/csharp.yaml")),
+        SupportLang::C => Some(include_str!("../langs/c.yaml")),
+        SupportLang::Cpp => Some(include_str!("../langs/cpp.yaml")),
+        SupportLang::Scala => Some(include_str!("../langs/scala.yaml")),
+        SupportLang::Bash => Some(include_str!("../langs/bash.yaml")),
+        SupportLang::Elixir => Some(include_str!("../langs/elixir.yaml")),
+        SupportLang::Lua => Some(include_str!("../langs/lua.yaml")),
+        SupportLang::Swift => Some(include_str!("../langs/swift.yaml")),
+        SupportLang::Zig => Some(include_str!("../langs/zig.yaml")),
         _ => None,
     }
 }
